a9bbcf806ed7fd40645a8fc58e9d53eca07f3d6f
Zane <zagrismore@gmail.com>
2025-02-05 13:17:56 -0500

    Smooth F/B

--- Scripts/Cars/parentCar.gd
+++ Scripts/Cars/parentCar.gd
@@ -2,8 +2,8 @@ extends CharacterBody2D
 class_name Car
 
 
-@export var baseAcceleration=10
-@export var baseTopSpeed=300
+@export var baseAcceleration=20
+@export var baseTopSpeed=400
 @export var baseTurnSpeed=0
 @export var baseTurnPower=0
 
@@ -18,7 +18,7 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("p1_up", "p1_down")
 	if direction:
-		velocity.y = move_toward(velocity.y, baseTopSpeed, baseAcceleration)
+		velocity.y = move_toward(velocity.y, baseTopSpeed*direction, baseAcceleration)
 	else:
 		velocity.y = move_toward(velocity.y, 0, baseDecel)
 

9f5d915eb5f5d5d8061210a30e44fb857f45f509
Zane <zagrismore@gmail.com>
2025-02-07 12:37:02 -0500

    Updated Car

--- Scripts/Cars/parentCar.gd
+++ Scripts/Cars/parentCar.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 class_name Car
 
-
+#The variables that change based on cars
 @export var baseAcceleration=20
 @export var baseTopSpeed=400
 @export var baseTurnSpeed=0
@@ -9,7 +9,8 @@ class_name Car
 
 #The base deceleration value
 var baseDecel=5
-
+#Stores the speed of the car
+var currentSpeed:float =0
 
 
 func _physics_process(delta):
@@ -18,10 +19,11 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("p1_up", "p1_down")
 	if direction:
-		velocity.y = move_toward(velocity.y, baseTopSpeed*direction, baseAcceleration)
+		currentSpeed = move_toward(velocity.y, baseTopSpeed*direction, baseAcceleration)
 	else:
-		velocity.y = move_toward(velocity.y, 0, baseDecel)
+		currentSpeed = move_toward(velocity.y, 0, baseDecel)
 
+	velocity=(currentSpeed*cos(rotation),currentSpeed*sin(rotation))
 	move_and_slide()
 
 func _input(event):

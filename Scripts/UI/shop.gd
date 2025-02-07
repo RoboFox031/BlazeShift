baa3e2ab8d38ed2837a10b8ab73e4ca17625716d
owilling <owilling@ftstudent.org>
2025-02-07 10:34:42 -0500

    added shop
    -created shop menu
    -color selection option
    -car selection option(no sprites yet)
    -coin hub(no functionality yet)

---
+++ Scripts/UI/shop.gd
@@ -0,0 +1,176 @@
+extends UI
+class_name shop
+
+var pOneOptionSelected = "car"
+var pTwoOptionSelected = "car"
+
+var pOneColorSelected = 0
+var pTwoColorSelected = 0
+
+var pOneCarSelection = 0
+var pTwoCarSelection = 0
+
+@onready var pOneRightArrow = $pOneRightArrow
+@onready var pOneLeftArrow = $pOneLeftArrow
+
+@onready var pTwoRightArrow = $pTwoRightArrow
+@onready var pTwoLeftArrow = $pTwoLeftArrow
+
+@onready var pOneCarOne = $pOneCarOne
+@onready var pOneCarTwo = $pOneCarTwo
+@onready var pOneCarThree = $pOneCarThree
+
+@onready var pTwoCarOne = $pTwoCarOne
+@onready var pTwoCarTwo = $pTwoCarTwo
+@onready var pTwoCarThree = $pTwoCarThree
+
+@onready var pOneWhite = $pOneWhiteCircle
+@onready var pOneBlack = $pOneBlackCircle
+@onready var pOneRed = $pOneRedCircle
+@onready var pOneOrange = $pOneOrangeCircle
+@onready var pOneYellow = $pOneYellowCircle
+@onready var pOneGreen = $pOneGreenCircle
+@onready var pOneBlue = $pOneBlueCircle
+@onready var pOnePurple = $pOnePurpleCircle
+
+@onready var pTwoWhite = $pTwoWhiteCircle
+@onready var pTwoBlack = $pTwoBlackCircle
+@onready var pTwoRed = $pTwoRedCircle
+@onready var pTwoOrange = $pTwoOrangeCircle
+@onready var pTwoYellow = $pTwoYellowCircle
+@onready var pTwoGreen = $pTwoGreenCircle
+@onready var pTwoBlue = $pTwoBlueCircle
+@onready var pTwoPurple = $pTwoPurpleCircle
+
+var pOneColors: Array
+var pTwoColors: Array
+var pOneCars: Array
+var pTwoCars: Array
+
+func _ready() -> void:
+	pOneColors = [pOneWhite, pOneBlack, pOneRed, pOneOrange, pOneYellow, pOneGreen, pOneBlue, pOnePurple]
+	pTwoColors = [pTwoWhite, pTwoBlack, pTwoRed, pTwoOrange, pTwoYellow, pTwoGreen, pTwoBlue, pTwoPurple]
+	pOneCars = [pOneCarOne, pOneCarTwo, pOneCarThree]
+	pTwoCars = [pTwoCarOne, pTwoCarTwo, pTwoCarThree]
+	_updateCarDisplay(pOneCars[pOneCarSelection],pTwoCars[pTwoCarSelection])
+	_updateColorDisplay(pOneColors[pOneColorSelected],pTwoColors[pTwoColorSelected])
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("p1_down") or Input.is_action_just_pressed("p1_up"):
+		if pOneOptionSelected == "car":
+			pOneOptionSelected = "color"
+			for c in pOneColors:
+				if c == pOneColors[pOneColorSelected]:
+					c.get_child(0).visible = true
+				else:
+					c.get_child(0).visible = false
+				pOneLeftArrow.get_child(0).visible = false
+				pOneRightArrow.get_child(0).visible = false
+		else:
+			pOneOptionSelected = "car"
+			for c in pOneColors:
+				c.get_child(0).visible = false
+			pOneLeftArrow.get_child(0).visible = true
+			pOneRightArrow.get_child(0).visible = true
+	if Input.is_action_just_pressed("p2_down") or Input.is_action_just_pressed("p2_up"):
+		if pTwoOptionSelected == "car":
+			pTwoOptionSelected = "color"
+			for c in pTwoColors:
+				if c == pTwoColors[pTwoColorSelected]:
+					c.get_child(0).visible = true
+				else:
+					c.get_child(0).visible = false
+				pTwoLeftArrow.get_child(0).visible = false
+				pTwoRightArrow.get_child(0).visible = false
+		else:
+			pTwoOptionSelected = "car"
+			for c in pTwoColors:
+				c.get_child(0).visible = false
+			pTwoLeftArrow.get_child(0).visible = true
+			pTwoRightArrow.get_child(0).visible = true
+	if Input.is_action_just_pressed("p1_right"):
+		if pOneOptionSelected == "car":
+			if pOneCarSelection + 1 < len(pOneCars):
+				pOneCarSelection += 1
+			else:
+				pOneCarSelection = 0
+			_updateCarDisplay(pOneCars[pOneCarSelection],pTwoCars[pTwoCarSelection])
+		elif pOneOptionSelected == "color":
+			if pOneColorSelected + 1 < len(pOneColors):
+				pOneColorSelected += 1
+			else:
+				pOneColorSelected = 0
+			_updateColorDisplay(pOneColors[pOneColorSelected],pTwoColors[pTwoColorSelected])
+	if Input.is_action_just_pressed("p1_left"):
+		if pOneOptionSelected == "car":
+			if pOneCarSelection - 1 >= 0:
+				pOneCarSelection -= 1
+			else:
+				pOneCarSelection = len(pOneCars) - 1
+			_updateCarDisplay(pOneCars[pOneCarSelection],pTwoCars[pTwoCarSelection])
+		elif pOneOptionSelected == "color":
+			if pOneColorSelected - 1 >= 0:
+				pOneColorSelected -= 1
+			else:
+				pOneColorSelected = len(pOneColors) - 1
+			_updateColorDisplay(pOneColors[pOneColorSelected],pTwoColors[pTwoColorSelected])
+	if Input.is_action_just_pressed("p2_right"):
+		if pTwoOptionSelected == "car":
+			if pTwoCarSelection + 1 < len(pTwoCars):
+				pTwoCarSelection += 1
+			else:
+				pTwoCarSelection = 0
+		elif pTwoOptionSelected == "color":
+			if pTwoColorSelected + 1 < len(pTwoColors):
+				pTwoColorSelected += 1
+			else:
+				pTwoColorSelected = 0
+			_updateColorDisplay(pOneColors[pOneColorSelected],pTwoColors[pTwoColorSelected])
+		_updateCarDisplay(pOneCars[pOneCarSelection],pTwoCars[pTwoCarSelection])
+	if Input.is_action_just_pressed("p2_left"):
+		if pTwoOptionSelected == "car":
+			if pTwoCarSelection - 1 >= 0:
+				pTwoCarSelection -= 1
+			else:
+				pTwoCarSelection = len(pTwoCars) - 1
+			_updateCarDisplay(pOneCars[pOneCarSelection],pTwoCars[pTwoCarSelection])
+		elif pTwoOptionSelected == "color":
+			if pTwoColorSelected - 1 >= 0:
+				pTwoColorSelected -= 1
+			else:
+				pTwoColorSelected = len(pTwoColors) - 1
+			_updateColorDisplay(pOneColors[pOneColorSelected],pTwoColors[pTwoColorSelected])
+
+func _updateCarDisplay(pOneCar,pTwoCar):
+	for c in pOneCars:
+		if c == pOneCar:
+			c.visible = true
+		else:
+			c.visible = false
+	for c in pTwoCars:
+		if c == pTwoCar:
+			c.visible = true
+		else:
+			c.visible = false
+
+func _updateColorDisplay(pOneColor,pTwoColor):
+	if pOneOptionSelected == "color":
+		for c in pOneColors:
+			if c == pOneColor:
+				c.scale.x = .15
+				c.scale.y = .15
+				c.get_child(0).visible = true
+			else:
+				c.scale.x = .088
+				c.scale.y = .088
+				c.get_child(0).visible = false
+	if pTwoOptionSelected == "color":
+		for c in pTwoColors:
+			if c == pTwoColor:
+				c.scale.x = .15
+				c.scale.y = .15
+				c.get_child(0).visible = true
+			else:
+				c.scale.x = .088
+				c.scale.y = .088
+				c.get_child(0).visible = false

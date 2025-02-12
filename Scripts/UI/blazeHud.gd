ce892893f1074d300d9bfb45f13a63091036567c
owilling <owilling@ftstudent.org>
2025-02-12 12:31:47 -0500

    changes

--- Scripts/UI/blazeHud.gd
+++ Scripts/UI/blazeHud.gd
@@ -11,6 +11,6 @@ func _ready() -> void:
 	blazeBar.value = blazeCurrent
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("p1_x") and blazeCurrent - blazeBurnSpeed * delta >= 0:
+	if Input.is_action_pressed("p2_x") and blazeCurrent - blazeBurnSpeed * delta >= 0:
 		blazeCurrent -= blazeBurnSpeed * delta
 	blazeBar.value = blazeCurrent

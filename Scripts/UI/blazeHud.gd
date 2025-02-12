b5de74432aed819ac8f5c5fd210b606f82747ced
owilling <owilling@ftstudent.org>
2025-02-12 12:45:19 -0500

    added blazehud

--- Scripts/UI/blazeHud.gd
+++ Scripts/UI/blazeHud.gd
@@ -9,8 +9,3 @@ var blazeBurnSpeed = 25
 func _ready() -> void:
 	blazeBar.max_value = blazeMax
 	blazeBar.value = blazeCurrent
-
-func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("p2_x") and blazeCurrent - blazeBurnSpeed * delta >= 0:
-		blazeCurrent -= blazeBurnSpeed * delta
-	blazeBar.value = blazeCurrent

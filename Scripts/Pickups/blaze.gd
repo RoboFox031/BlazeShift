6c73bd3c84046e6114b43cf21fdef9d0f15b4b67
owilling <owilling@ftstudent.org>
2025-02-13 12:42:39 -0500

    blaze pickup power

---
+++ Scripts/Pickups/blaze.gd
@@ -0,0 +1,8 @@
+extends pickup
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is playerOneCar:
+		entered(body)
+	elif body is playerTwoCar:
+		entered(body)
--- Scripts/globalVars.gd
+++ Scripts/globalVars.gd
@@ -1,3 +1,5 @@
 extends Node
 
 var track = null
+var pOnePowerup = null
+var pTwoPowerup = null

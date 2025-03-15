c497a03f6250b9654850e19b18f246b0b995eda1
hucu314 <hcumpean@ftstudent.org>
2025-03-14 23:00:24 -0400

    Fixed the tile set map and rural track

--- Scripts/globalVars.gd
+++ Scripts/globalVars.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var track = preload("res://Scenes/Tracks/basicTrack.tscn")
+var track = preload("res://Scenes/Tracks/ruralTrack.tscn")
 var playerOneCar = preload("res://Scenes/Cars/lemkeCar.tscn")
 var playerTwoCar = preload("res://Scenes/Cars/lemkeCar.tscn")
 var playerOneCarSprite = null

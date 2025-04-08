f06fb47e42d8901531b5135b94a7ce1400699b0f
MonkeySword441 <kylepurdy864@gmail.com>
2025-04-08 12:53:26 -0400

    added powerup boxes to maps, yet to fix powerup selection

--- Scripts/Pickups/powerup_box.gd
+++ Scripts/Pickups/powerup_box.gd
@@ -7,9 +7,9 @@ class_name pickupBox
 
 func _on_area_2d_body_entered(body: Car):
 	if randi_range(1,2) == 1:
-		globalVars.pOnePowerup.type = 'fireball'
-		globalVars.pTwoPowerup.type = 'fireball'
+		globalVars.pOnePowerup.type = fireballPickup
+		globalVars.pTwoPowerup.type = fireballPickup
 	elif randi_range(1,2) == 2:
-		globalVars.pOnePowerup.type = 'blaze'
-		globalVars.pTwoPowerup.type = 'blaze'
+		globalVars.pOnePowerup.type = 'blazePickup'
+		globalVars.pTwoPowerup.type = 'blazePickup'
 	

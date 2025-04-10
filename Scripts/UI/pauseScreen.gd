c8d5b28e5164638423298abdb289c8205f249168
owilling <owilling@ftstudent.org>
2025-04-10 08:33:08 -0400

    updated pause menu

--- Scripts/UI/pauseScreen.gd
+++ Scripts/UI/pauseScreen.gd
@@ -1,7 +1,6 @@
 extends Node2D
  
 @onready var pauseLabel = $pauseLabel
-@onready var timer = $timer
 
 var inControl: int = 0
 
@@ -67,6 +66,18 @@ func _physics_process(delta: float) -> void:
 		else:
 			selected = 0
 		_updateMenu()
+	if Input.is_action_just_pressed('p1_x'):
+		if inControl == 3:
+			inControl = 1
+			$pOneConfirmLabel.visible = false
+			$pTwoConfirmLabel.visible = false
+			_updateMenu()
+	if Input.is_action_just_pressed('p2_x'):
+		if inControl == 3:
+			inControl = 2
+			$pOneConfirmLabel.visible = false
+			$pTwoConfirmLabel.visible = false
+			_updateMenu()
 		
 func _updateMenu():
 	if inControl != 0:
@@ -75,6 +86,7 @@ func _updateMenu():
 		self.visible = false
 		
 	if $pOneConfirmLabel.visible == true:
+		$infoLabel.text = 'press x to cancel'
 		if pOneConfirm == true:
 			$pOneConfirmLabel.text = 'press start again to undo'
 			$pOneConfirmLabel/label.text = 'confirmed'
@@ -91,6 +103,8 @@ func _updateMenu():
 			$pTwoConfirmLabel.text = 'press start to confirm'
 			$pTwoConfirmLabel/label.text = 'unconfirmed'
 			$pTwoConfirmLabel/label.add_theme_color_override("font_color", Color(1,0,0))
+	else:
+		$infoLabel.text = 'press start to select'
 	
 	if pOneConfirm == true and pTwoConfirm == true:
 		_restart()
@@ -117,8 +131,3 @@ func _useButton(option):
 		
 func _restart():
 	get_tree().change_scene_to_file("res://Scenes/UI/titleScreen.tscn")
-		
-
-
-func _on_timer_timeout() -> void:
-	timer.start()

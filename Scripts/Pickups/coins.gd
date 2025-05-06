e57a009d039569cab55a2c6e590db6b40c94e6bb
MonkeySword441 <kylepurdy864@gmail.com>
2025-05-06 13:17:26 -0400

    added powerup boxes and coins to basic map

--- Scripts/Pickups/coins.gd
+++ Scripts/Pickups/coins.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+@onready var timer = $timer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,11 +12,17 @@ func _process(delta):
 
 
 func _on_area_2d_body_entered(body):
+	timer.start()
 	if body.currentOwner == body.playerChoices.p1:
 		globalVars.pOneLastRaceCoinsCollected += 1
-		queue_free()
+		self.visible = false
 	if body.currentOwner == body.playerChoices.p2:
 		globalVars.pTwoLastRaceCoinsCollected += 1
-		queue_free()
+		self.visible = false
 	#globalVars.p1Coin
 	pass # Replace with function body.
+
+
+func _on_timer_timeout() -> void:
+	self.visible = true
+	
